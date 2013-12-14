5034448408880b495ea7d37fe5f71382d3fce284
John Louis Walker <injyuw@gmail.com>
2013-12-14 15:55:26 -0500

    Add naive initialization loop for mpz_primes.

--- src/quadraticsieve.cc
+++ src/quadraticsieve.cc
@@ -1,10 +1,8 @@
 #include <assert.h>
 #include <stdio.h>
-
+#include "primes.h"
 #include "utility.h"
 #include "quadraticsieve.h"
-
-
 
 int quadratic_sieve(mpz_t result, mpz_t composite)
 {
@@ -13,7 +11,19 @@ int quadratic_sieve(mpz_t result, mpz_t composite)
   mpz_set_ui(prime, 7);
 
   int res = mpz_legendre(composite, prime);
+
   
+  
+  // NOTE: mpz_primes[i] shouldn't ever be larger than the number being factored
+  const size_t mpz_primes_length = primes_length;
+  mpz_t mpz_primes[mpz_primes_length];
+
+  for(size_t i = 0; i < mpz_primes_length; i++)
+    {
+      mpz_init(mpz_primes[i]);
+      mpz_set_ui(mpz_primes[i], primes[i]);
+    }
+
   mpz_t a, b;
   mpz_init(a);
   mpz_init(b);
