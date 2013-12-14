c33cf663bae87c7d2016e8b7fd7c97d327b6609c
John Louis Walker <injyuw@gmail.com>
2013-12-14 12:20:41 -0500

    Nevermind, it's GMP the first time around.

--- src/quadraticsieve.cc
+++ src/quadraticsieve.cc
@@ -4,9 +4,14 @@
 #include "utility.h"
 #include "quadraticsieve.h"
 
+long long legendre_symbol(long long a, long long p)
+{
+  return (a ^ ((p - 1) / 2)) % p;
+}
+
 void quadratic_sieve(long long& result, long long input, int& err)
 {
-  
+  printf("%lld", legendre_symbol(input, 2));
 }
 
 
