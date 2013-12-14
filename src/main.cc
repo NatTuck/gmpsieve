5edd1d5feee0864691c2d23d3fda489d9aeeee84
John Louis Walker <injyuw@gmail.com>
2013-12-13 23:21:30 -0500

    Increment to version 0.0.2
    Remove FermatFactor with macros.
    
    Implement input in main(...) for long longs.
    In general, it's easier to prototype.

--- src/main.cc
+++ src/main.cc
@@ -1,13 +1,18 @@
 #include <assert.h>
 #include <gmp.h>
+#include <limits.h>
 #include <mpi.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "fermatfactor.h"
 #include "utility.h"
 
 #define BUFFLEN 1024
 #define DEBUG 1
-
+#define FERMAT 0 
+#define QUADRATICSIEVENOGMP 1
+	  
+#if FERMAT == 1
 int main(int argc, char *argv[])
 {
   int err;
@@ -37,3 +42,32 @@ int main(int argc, char *argv[])
   mpz_fermat(result, n);
   return 0;
 }
+#endif
+#if QUADRATICSIEVENOGMP == 1
+int main(int argc, char *argv[])
+{
+  char input[BUFFLEN];
+  long long n; 
+  { // Read input and float
+	  
+    if (DEBUG)
+      printf("Enter your number: ");
+
+    fgets(input, BUFFLEN, stdin);
+    for(int i = 0; i < BUFFLEN; i++)
+      {
+	if ('\n' == input[i])
+	  {
+	    input[i] = '\0';
+	    break;
+	  }
+      }
+    
+    {// atoll is garbage. Try to find strtonum from FreeBSD
+      n = atoll(input); 
+    }
+  }
+  printf("%lld\n", n);
+  return 0;
+}
+#endif
