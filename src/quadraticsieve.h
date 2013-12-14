11c03da0c30fd05cef9e548d909313a0cb946e55
John Louis Walker <injyuw@gmail.com>
2013-12-13 23:45:18 -0500

    Declare the quadratic_sieve(...) procedure.

---
+++ src/quadraticsieve.h
@@ -0,0 +1,11 @@
+#ifndef __QUADRATICSIEVE_H__
+#define __QUADRATICSIEVE_H__
+// stub
+//#include <gmp.h>
+//
+//
+// void mpz_quadratic_sieve(mpz_t result, mpz_t input){};
+
+void quadratic_sieve(long long& result, long long input, int& err);
+
+#endif
